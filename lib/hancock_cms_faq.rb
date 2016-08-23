a7fb7f45859e143cd125b2b388a2416e4dee0758
Alexander Kiseliev <i43ack@gmail.com>
2016-08-23 13:07:53 +0300

    cms plugin registration

--- lib/hancock_cms_faq.rb
+++ lib/hancock_cms_faq.rb
@@ -6,25 +6,7 @@ require 'hancock/faq/configuration'
 require 'hancock/faq/engine'
 
 module Hancock::Faq
-  # Hancock::register_plugin(self)
-
-  class << self
-    def orm
-      :mongoid
-    end
-    def mongoid?
-      Hancock::Faq.orm == :mongoid
-    end
-    def active_record?
-      Hancock::Faq.orm == :active_record
-    end
-    def model_namespace
-      "Hancock::Faq::Models::#{Hancock::Faq.orm.to_s.camelize}"
-    end
-    def orm_specific(name)
-      "#{model_namespace}::#{name}".constantize
-    end
-  end
+  include Hancock::Plugin
 
   autoload :Admin,  'hancock/faq/admin'
   module Admin
