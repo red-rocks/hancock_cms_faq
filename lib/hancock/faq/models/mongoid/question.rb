7af7fc0d14c8660e1a644853df00c70f76f386da
Alexander Kiseliev <i43ack@gmail.com>
2017-03-10 14:03:50 +0300

    updater field fix

--- lib/hancock/faq/models/mongoid/question.rb
+++ lib/hancock/faq/models/mongoid/question.rb
@@ -36,19 +36,35 @@ module Hancock::Faq
           scope :not_answered,  -> { where(answered: false) }
 
           scope :recent, ->(count = 5) { enabled.after_now.by_date.limit(count) }
+
+
+          def category
+            self.categories.enabled.sorted.first
+          end
+
+          def question_text_output
+            self.question_text_after_editing.blank? ? self.question_text : self.question_text_after_editing
+          end
+
+          def author_name_output
+            self.author_name_text_after_editing.blank? ? self.author_name : self.author_name_text_after_editing
+          end
+
+
+          if Hancock.rails5?
+            if relations.has_key?("updater") and defined?(::Mongoid::History)
+              belongs_to :updater, class_name: ::Mongoid::History.modifier_class_name, optional: true, validate: false
+              _validators.delete(:updater)
+              _validate_callbacks.each do |callback|
+                if callback.raw_filter.respond_to?(:attributes) and callback.raw_filter.attributes.include?(:updater)
+                  _validate_callbacks.delete(callback)
+                end
+              end
+            end
+          end
+
         end
 
-        def category
-          self.categories.enabled.sorted.first
-        end
-
-        def question_text_output
-          self.question_text_after_editing.blank? ? self.question_text : self.question_text_after_editing
-        end
-
-        def author_name_output
-          self.author_name_text_after_editing.blank? ? self.author_name : self.author_name_text_after_editing
-        end
 
       end
     end
